12d6ab6dbff318e56011a5239a8d73f7d9397aa2
Tamas Biro <tamas.biro@liferay.com>
2025-02-13 11:32:47 +0100

    Revert "INFOSEC-3209 Trying to insert the UserInfo, but fails with "Unresolved requirement: Import-Package: com.liferay.portal.kernel.service; version="37.1.0" ""
    This reverts commit d5bb66cd02ab0c23e27af2518dc5768c041a72e6.

--- modules/my-groovy-executor/src/main/java/com/example/GroovyExecutor.java
+++ modules/my-groovy-executor/src/main/java/com/example/GroovyExecutor.java
@@ -1,9 +1,6 @@
 package com.example;
 
-import com.liferay.portal.kernel.exception.PortalException;
-import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
-import groovy.lang.Script;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
@@ -18,7 +15,7 @@ import java.nio.file.Paths;
 )
 public class GroovyExecutor {
 
-	private static final String SCRIPT_PATH = "c:\\git\\bundles\\7.4.3.125-ga125\\scripts\\create_user.groovy";
+	private static final String SCRIPT_PATH = "c:\\git\\bundles\\7.4.3.125-ga125\\scripts\\myscript.groovy";
 
 	@Activate
 	public void activate() {
@@ -33,37 +30,11 @@ public class GroovyExecutor {
 
 		try {
 			String scriptContent = new String(Files.readAllBytes(Paths.get(SCRIPT_PATH)));
-
-			_executeGroovyScript(_createBinding(), scriptContent);
-
+			GroovyShell shell = new GroovyShell();
+			shell.evaluate(scriptContent);
 			System.out.println("Groovy script finished.");
-
-		} catch (IOException | PortalException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
 		}
-	}
-
-	private void _executeGroovyScript(
-			Binding binding, String script)
-			throws Exception {
-
-		GroovyShell groovyShell = new GroovyShell(binding);
-
-		Script compiledScript = groovyShell.parse(script);
-
-		//compiledScript.setBinding(new Binding(inputObjects));
-
-		compiledScript.run();
-	}
-
-	private Binding _createBinding() throws PortalException {
-
-		Binding binding = new Binding();
-
-		binding.setVariable("user", com.liferay.portal.kernel.service.UserLocalServiceUtil.getUserById(20123));
-
-		return binding;
 	}
 }
